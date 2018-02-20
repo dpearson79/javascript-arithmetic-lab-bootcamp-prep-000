70ba9f71587d11c65ebb26d645fe35b84fa7d2ae
Duane Pearson <dp.anarchyink@gmail.com>
2018-02-20 12:52:18 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,48 @@
+var a, b 
+a = 1000
+b = 15
+function add(a, b) {
+ return (a + b) 
+}
+var a, b
+a = -816
+b = 3 
+function subtract(a, b) {
+  return (a - b)
+}
+var a, b
+a = 119052
+b = 5 
+function multiply(a, b) {
+  return (a * b)
+}
+var a, b
+a = 0.3076923076923076
+b = 2
+function divide(a, b) {
+  return (a / b)
+} 
+var n = 200
+function inc(n) {
+return (n + 1)
+  }
+var n = 58
+function dec(n) {
+  return (n - 1)
+}
+var n = '2.2222'
+function makeInt(n) {
+  return
+}
+var n = 2328
+function makeInt(n) {
+  parseInt('0 * 2328', 10)
+  return
+}
+var n = '2.222'
+function preserveDecimal(n) {
+  parseFloat(n)
+  return
+}
+
+
